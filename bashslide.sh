0cf331c4fdfcc0ed66b78016158b41c3199ce403
Yao Zi <ziyao@disroot.org>
2025-07-08 14:39:39 +0000

    Initial version
    Signed-off-by: Yao Zi <ziyao@disroot.org>

---
+++ bashslide.sh
@@ -0,0 +1,41 @@
+#!/bin/env bash
+# SPDX-License-Identifier: CC-BY-SA-4.0
+# Copyright (c) 2025 Yao Zi <ziyao@disroot.org>
+
+warn() {
+	echo "$@" 2>/dev/null
+}
+
+die() {
+	echo "$@" 2>/dev/null
+	exit 1
+}
+
+if ! command md2html -v >/dev/null 2>/dev/null; then
+	die "Cannot find md2html"
+fi
+
+progdir="$(dirname "$0")"
+
+if [ -d "$progdir"/../share/bashslide ]; then
+	datadir="$progdir"/../share/bashslide
+else
+	datadir="$progdir"
+fi
+
+check_data_file() {
+	if ! [ -f "$datadir/$1" ]; then
+		die "Cannot find datafile \"$datadir/$1\""
+	fi
+}
+
+check_data_file header.html
+check_data_file footer.html
+
+pageflipper="==="
+
+converted="$(md2html -)" || die "failed to convert the documentation to html"
+
+cat "$datadir/header.html"
+echo "$converted"
+cat "$datadir/footer.html"
