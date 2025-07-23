b780c5e3c3c376182ca3fa386abf9d5236e09369
Yao Zi <ziyao@disroot.org>
2025-07-23 07:40:43 +0000

    Support embedding resources in the result HTML
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- bashslide.sh
+++ bashslide.sh
@@ -38,6 +38,36 @@ mdargs="--fstrikethrough"
 converted="$(md2html $mdargs -)" ||
 	die "failed to convert the documentation to html"
 
+paths=()
+
 cat "$datadir/header.html"
+
+while read line; do
+	if echo "$line" | grep -q -o -- '$$[^$[:cntrl:]]\+\$\$'; then
+		path=("$(echo "$line"				|
+		       grep -o -- '$$[^$[:cntrl:]]\+\$\$'	|
+		       sed -e 's/\$\$//g')")
+
+		if ! [ -e "$path" ]; then
+			die "Embedded file '$path' doesn't exist";
+		fi
+
+		paths+=("$path")
+		_path=("$(echo "$path" | sed -e 's/\//\\\//g')")
+		echo -nE "s/\\\$\\\$$_path\\\$\\\$/data:" > "$path.rep"
+		file -b --mime-type $path | sed -e 's/\//\\\//g' |
+			tr -d '\n' >> "$path.rep"
+		echo -nE ";base64," >> "$path.rep"
+		base64 -w 0 "$path" | sed -e 's/\//\\\//g' >> "$path.rep"
+		echo -nE '/g' >> "$path.rep"
+	fi
+done <<< $(echo "$converted")
+
+for ((i=0;i<"${#paths[@]}";i++)); do
+	path=${paths[i]}
+	converted="$(echo "$converted" | sed -f "$path.rep")"
+done
+
 echo "$converted"
+
 cat "$datadir/footer.html"
